5bc63ff6367aed4c82636285eae4d18309b8301c
Ellie Parobek <ellie.parobek@gmail.com>
2020-02-11 12:10:22 -0500

    plist example finished, autolayout example added

--- PListTutorial/PListTutorial/ViewController.swift
+++ PListTutorial/PListTutorial/ViewController.swift
@@ -19,11 +19,55 @@ class ViewController: UIViewController {
     var phoneNumbers: [String] = []
     
     @IBAction func saveData(sender: UIButton) {
+        if let documentsPathURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+                   var plistPath = documentsPathURL.appendingPathComponent("data.plist")
+            
+            // Get info from GUI and save it
+            personName = nameEntered.text!
+            phoneNumbers = [homePhone.text!, workPhone.text!, cellPhone.text!]
+            
+            let plistDict: Dictionary<String, Any> = [
+                "Name": personName,
+                "Phones": phoneNumbers
+            ]
+            
+            do {
+                let plistData = try PropertyListSerialization.data(fromPropertyList: plistDict, format: .xml, options: 0)
+                
+                try plistData.write(to: plistPath)
+            }
+            catch {
+                print(error)
+            }
+        }
         
+        // Dismiss keyboard whenever you have multiple fields
+        view.endEditing(true)
+    }
+    
+    @IBAction func deleteData(sender: UIButton) {
+        let fileMgr = FileManager.default
+        
+        let dir = fileMgr.urls(for: .documentDirectory, in: .userDomainMask).first
+        
+        // Show contents of the directory
+        print("Before deleting: \(try? fileMgr.contentsOfDirectory(atPath: dir!.path))")
+        
+        let fileURL = dir!.appendingPathComponent("data.plist")
+        
+        do {
+            try fileMgr.removeItem(at: fileURL)
+        }
+        catch {
+            print(error)
+        }
+        
+        print("After deleting: \(try? fileMgr.contentsOfDirectory(atPath: dir!.path))")
     }
     
     @IBAction func textFieldReturn(sender: UITextField) {
-        
+        // Dismiss keyboard when user presses return / done
+        sender.resignFirstResponder()
     }
 
     override func viewDidLoad() {
