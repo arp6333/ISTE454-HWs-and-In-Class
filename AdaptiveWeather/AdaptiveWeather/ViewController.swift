eec0c4ca1237677396ae108e7d09f22347c0fb04
Ellie Parobek <ellie.parobek@gmail.com>
2020-02-18 11:51:31 -0500

    Gallery completed

---
+++ AdaptiveWeather/AdaptiveWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdaptiveWeather
+//
+//  Created by Ellie on 2/18/20.
+//  Copyright © 2020 Ellie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
